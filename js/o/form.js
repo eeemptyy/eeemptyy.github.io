38e47d005dc894575d45e12cd961a3dbb47f9bb0
Jompol Sermsook <jompol.s@ku.th>
2017-07-29 02:34:44 +0700

    form o

---
+++ js/o/form.js
@@ -0,0 +1,10 @@
+var database = firebase.database();
+// database.ref('users/123').set({ username: 'EMPTYY', email: "jompol@kk.com" });
+
+// var ref = firebase.database().ref();
+
+// ref.on("value", function(snapshot) {
+//    console.log(snapshot.val());
+// }, function (error) {
+//    console.log("Error: " + error.code);
+// });
